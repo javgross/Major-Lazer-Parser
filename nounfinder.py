42a239bb8e59d4d0f86d2ba29c4800a577b964a8
javgross <javgross@gmail.com>
2018-11-11 23:30:02 -0400

    first commit

---
+++ nounfinder.py
@@ -0,0 +1,11 @@
+def nounfinder():
+	import nltk
+	tokens = nltk.word_tokenize(sentence)
+	tagged = nltk.pos_tag(tokens)
+	length = len(tagged)
+	a = list()
+	for i in range (0, length):
+		log = (tagged [i][1][0] == 'N')
+		if log == True:
+			a.append(tagged [i][0])
+	print (a)
